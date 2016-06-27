6adb9e1e194d76576f3465065288ce002c90a9e3
Art <lubkovskij@gmail.com>
2016-06-27 20:22:03 +0000

    Added ability to find user device location, country

---
+++ somethingNew.ts
@@ -0,0 +1,54 @@
+function getCurrentDeviceLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(displayLocation);
+    }
+    else {
+        alert("Sorry ;P Something wrong on this way");
+    }
+}
+
+function displayLocation(position) {
+    
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var textfieldLatitude = document.getElementById("latitude");
+    var textfieldLongitude = document.getElementById("longitude");
+    textfieldLatitude.innerHTML = "Latitude: " + latitude;
+    textfieldLongitude.innerHTML = "Longitude: " + longitude;
+}
+function getCountry() {
+    console.error("getCountryFrom");
+    getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyA-lDzccV2juv39_BrE-sRkPZ-NqaMaWD0");
+}
+function getJSON(url) {
+    console.error("gettingJSON");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        console.error("state are ready");
+        var myArr = JSON.parse(xmlhttp.responseText);
+        countryOutput(myFunction(myArr));
+    };
+    xmlhttp.open("POST", url, true);
+    xmlhttp.send();
+}
+
+function myFunction(arr) {
+    console.error("reading parced JSON");
+    var out = "";
+    var i;
+    for(i = 0; i < arr.results.length; i++) {
+        console.error("reading results");
+        for(var index = 0; index < arr.results[i].address_components.length; index++) {
+            console.error("reading address components");
+            if(arr.results[i].address_components[index].types[0] == "country") {
+                console.error("YEAH");
+                return arr.results[i].address_components[index].short_name;
+            }
+        }
+    }
+    
+}
+
+function countryOutput(name) {
+    document.getElementById("resultTextarea").textContent = "Country: " + name;
+}
